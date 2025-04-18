8881b6961b5770b7950455c200d0117f750bf851
Mostafa Gad <m.gad192@gmail.com>
2025-04-19 00:48:59 +0200

    add api configuration

--- app/src/main/java/com/example/paymobtechnicaltask/data/utils/Constants.kt
+++ app/src/main/java/com/example/paymobtechnicaltask/data/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.example.paymobtechnicaltask.data.utils
 
 object Constants {
     const val API_KEY = "x-api-key"
-    const val NETWORK_TIMEOUT = 100000L
+    const val NETWORK_TIMEOUT = 30000L
 }
