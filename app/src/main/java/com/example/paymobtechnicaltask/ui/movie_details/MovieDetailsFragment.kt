00179d4570e723adf5a855fa4c549a7be86f99c0
Mostafa Gad <m.gad192@gmail.com>
2025-04-20 23:18:18 +0200

    add movie details layout

---
+++ app/src/main/java/com/example/paymobtechnicaltask/ui/movie_details/MovieDetailsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.paymobtechnicaltask.ui.movie_details
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.paymobtechnicaltask.R
+import com.example.paymobtechnicaltask.databinding.FragmentMovieDetailsBinding
+import com.example.paymobtechnicaltask.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieDetailsFragment : BaseFragment<FragmentMovieDetailsBinding>(FragmentMovieDetailsBinding::inflate) {
+
+
+}
---
+++ app/src/main/java/com/example/paymobtechnicaltask/ui/movie_details/MovieDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.paymobtechnicaltask.ui.movie_details
+
+import androidx.lifecycle.ViewModel
+
+class MovieDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
