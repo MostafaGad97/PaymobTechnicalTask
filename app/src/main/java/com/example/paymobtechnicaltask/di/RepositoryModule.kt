c448979aa8bb2167409f7e291141fb9de752c3c6
Mostafa Gad <m.gad192@gmail.com>
2025-07-17 15:07:38 +0300

    refactor repository module to use @Binds

--- app/src/main/java/com/example/paymobtechnicaltask/di/RepositoryModule.kt
+++ app/src/main/java/com/example/paymobtechnicaltask/di/RepositoryModule.kt
@@ -4,6 +4,7 @@ import com.example.paymobtechnicaltask.data.datasource.LocalDataSource
 import com.example.paymobtechnicaltask.data.datasource.RemoteDataSource
 import com.example.paymobtechnicaltask.data.repo_impl.MoviesRepositoryImpl
 import com.example.paymobtechnicaltask.domain.repository.MoviesRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -12,18 +13,12 @@ import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideMoviesRepository(
-        remoteDataSource: RemoteDataSource,
-        localDataSource: LocalDataSource
-    ): MoviesRepository {
-        return MoviesRepositoryImpl(
-            remoteDataSource = remoteDataSource,
-            localDataSource = localDataSource
-        )
-    }
+    abstract fun provideMoviesRepository(
+        impl: MoviesRepositoryImpl
+    ): MoviesRepository
 
 }
